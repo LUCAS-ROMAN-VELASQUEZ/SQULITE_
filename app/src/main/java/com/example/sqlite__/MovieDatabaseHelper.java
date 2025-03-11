df0a4758fd6f426bcc0eb594e92e6996d423e67c
LUCASFOKING <lucrack3ggg5@gmail.com>
2025-03-11 10:24:41 +0100

    Hola

--- app/src/main/java/com/example/sqlite__/MovieDatabaseHelper.java
+++ app/src/main/java/com/example/sqlite__/MovieDatabaseHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MovieDatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "movies.db";
+    private static final String DATABASE_NAME = "futbolistas.db";
     private static final int DATABASE_VERSION = 1;
 
     public MovieDatabaseHelper(Context context) {
@@ -15,15 +15,18 @@ public class MovieDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)");
+        db.execSQL("CREATE TABLE Futbolistas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, edad INTEGER, equipo TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS movies");
+        db.execSQL("DROP TABLE IF EXISTS Futbolistas");
         onCreate(db);
     }
 
     public void clearDatabase(SQLiteDatabase db) {
         db.execSQL("DELETE FROM movies");
+        db.execSQL("DELETE FROM Futbolistas");
     }
 }
